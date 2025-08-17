d8668837e738bee999e331ca540b0d56de6758c0
Nizaam Yunus <nizaamcoleyunus18@gmail.com>
2025-08-17 09:57:02 +0200

    Sign up form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 
 import { LoginReactiveFormsComponent } from "./auth/login-reactive-forms/login-reactive-forms.component";
+import { SignupComponent } from "./auth/signup/signup.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [LoginReactiveFormsComponent],
+  imports: [SignupComponent],
 })
 export class AppComponent {}
