badd0772f175fd738178a1ccf775b3f1e8ceaa48
Nizaam Yunus <nizaamcoleyunus18@gmail.com>
2025-08-17 11:20:55 +0200

    Connecting and registering inputs for a complex form

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -10,12 +10,17 @@ import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angula
 })
 export class SignupComponent {
   form = new FormGroup({
-    email: new FormControl('', {
-      validators: [Validators.email, Validators.required]
-    }),
-    password: new FormControl('', {
-      validators: [Validators.required, Validators.minLength(6)]
-    }),
+    email: new FormControl('', { validators: [Validators.email, Validators.required] }),
+    password: new FormControl('', { validators: [Validators.required, Validators.minLength(6)] }),
+    confirmPassword: new FormControl('', { validators: [Validators.required, Validators.minLength(6)] }),
+    firstName: new FormControl('', { validators: [Validators.required] }),
+    lastName: new FormControl('', { validators: [Validators.required] }),
+    street: new FormControl('', { validators: [Validators.required] }),
+    number: new FormControl('', { validators: [Validators.required] }),
+    postalCode: new FormControl('', { validators: [Validators.required] }),
+    city: new FormControl('', { validators: [Validators.required] }),
+    role: new FormControl<'student' | 'teacher' | 'employee' | 'founder' | 'other'>('student', { validators: [Validators.required] }),
+    agree: new FormControl(false, { validators: [Validators.required] }),
   });
 
   onSubmit() {
@@ -25,5 +30,5 @@ export class SignupComponent {
   onReset() {
     this.form.reset();
   }
-  
+
 }
